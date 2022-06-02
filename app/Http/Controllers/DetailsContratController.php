7528223adc1b3be62811fda5734a4b4d417a0a7e
stanic_wa <skakahoun@gmail.com>
2022-06-02 06:51:22 +0100

    contrat, detail-contrat model, controller, migration

---
+++ app/Http/Controllers/DetailsContratController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Details_Contrat;
+use Illuminate\Http\Request;
+
+class DetailsContratController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Details_Contrat  $details_Contrat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Details_Contrat $details_Contrat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Details_Contrat  $details_Contrat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Details_Contrat $details_Contrat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Details_Contrat  $details_Contrat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Details_Contrat $details_Contrat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Details_Contrat  $details_Contrat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Details_Contrat $details_Contrat)
+    {
+        //
+    }
+}
