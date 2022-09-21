d6fe5ae5095619fe80622471128a244e93a402eb
Thuret <tgenougan@gmail.com>
2022-09-21 09:28:21 +0100

    html2pdf vue ajouté

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,11 +8,13 @@ use App\Models\Contrat;
 use App\Models\Setting;
 use App\Helpers\herpers;
 use Illuminate\Http\Request;
-use Barryvdh\DomPDF\Facade\Pdf as PDF;
+use Spipu\Html2Pdf\Html2Pdf;
 
 
 // require _DIR_.'/vendor/autoload.php';
-use Spipu\Html2Pdf\Html2Pdf;
+use Barryvdh\DomPDF\Facade\Pdf as PDF;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
 
 class ReportController extends Controller
 {
@@ -122,132 +124,38 @@ class ReportController extends Controller
             'signature1' => $report->signatory1 == '1' ? $setting->signature1 : '',
             'signatory2' => $report->signatory2 == '1' ? $setting->signatory2 : '',
             'signature2' => $report->signatory2 == '1' ? $setting->signature2 : '',
-            'patient' => $report->patient->name,
+            'patient_firstname' => $report->patient->firstname,
+            'patient_lastname' => $report->patient->lastname,
+            'patient_age' => $report->patient->age,
+            'patient_genre' => $report->patient->genre,
+            'hospital_name' => $report->order->hospital->name,
+            'doctor_name' => $report->order->doctor->name,
             'date' => date('m/d/Y')
         ];
-        $html2pdf = new Html2Pdf();
-        // border: 3px solid #73AD21;
-        $html2pdf->setDefaultFont('Helvetica');
 
-        // $html2pdf->addFont($family, $style = '', $file = '');
-        $html2pdf->__construct(
-            $orientation = 'P',
-            $format = 'A4',
-            $lang = 'fr',
-            $unicode = true,
-            $encoding = 'UTF-8',
-            $margins = array(8, 20, 8, 8),
-            $pdfa = false
-        );
-        $html2pdf->writeHTML('
-            <div style="display:inline-block; ">
-                <span style="display: inline-block;padding-top: 5px; font-size:25px; margin-top:-50px "><img src="'.public_path('adminassets/images/Labo_Logo_1.png').'" alt=""></span> 
-                <div style="display: inline-block; padding: 5px; position: absolute; top:20px; right: 0px; padding: 10px; text-align:right;">
-                    <b><span style="font-size:17px; text-align:right;" >  CENTRE ADECHINA ANATOMIE PATHOLOGIE</span></b>
-                    <br><span style="font-size:10px; text-align:right;" >Laboratoire d’Anatomie Pathologique</span>
-                </div> 
-            </div>
-            <div style="display: inline-block; position: absolute; right: 0px;width: 150px;padding: 10px;  margin-top:-20px">
-                <p>
-                    <b>N° ANAPTH :</b> XXXXXX
-                    <br>
-                    <b>Date :</b> 14 Juillet 2022
-                </p>
-            </div> 
-            <div style="margin-top:20px; background-color:#0070C1; width:100%; height:50px;color:rgb(255,255,255); text-align: center; padding-top:19px;font-size:25px;">
-                <b>COMPTE RENDU HISTOPATHOLOGIE</b>
-            </div>
-            <br><br>
-            <div>
-                <fieldset style="border: 3px solid rgb(0,0,0,0)">
-                    <legend style="font-size: 1.5em; padding-bottom: 10px; padding-left: 5px; padding-right: 5px; border:none; background-color:rgb(255,255,255); ">
-                        <b>Informations prélèvement</b>
-                    </legend>
-                    <p style="margin-left:10px; margin-right:10px; display:block; width: 100%;">
-                    
+        try {
+            $content = view('pdf/canva', $data)->render();
+        
+            $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', 0);
+            $html2pdf->__construct(
+                $orientation = 'P',
+                $format = 'A4',
+                $lang = 'fr',
+                $unicode = true,
+                $encoding = 'UTF-8',
+                $margins = array(8, 20, 8, 8),
+                $pdfa = false
+            );
+            $html2pdf->writeHTML($content);
+            $newname= 'CO-'.$report->order->code.'.pdf';
+            $html2pdf->output($newname);
+            
+        } catch (Html2PdfException $e) {
+            $html2pdf->clean();
+        
+            $formatter = new ExceptionFormatter($e);
+            echo $formatter->getHtmlMessage();
+        }
 
-                       <table style="max-width: 100%;width: 500px ">
-                            <tbody>
-                                <tr>
-                                    <th>Nom :</th>
-                                    <td>'.$report->patient->firstname.' </td>
-                                    <th>Date prélèvement : </th>
-                                    <td>12 Juillet 2022 </td>
-                                </tr>
-                                <tr>
-                                    <th>Prénoms :</th>
-                                    <td>'.$report->patient->lastname.' </td>
-                                    <th>Date d’arrivée labo : </th>
-                                    <td>13 Juillet 2022 </td>
-                                </tr>
-                                <tr>
-                                    <th>Age :</th>
-                                    <td>'.$report->patient->age.' </td>
-                                    <th>Service demandeur  </th>
-                                    <td>'.$report->order->hospital->name.' </td>
-                                </tr>
-                                <tr>
-                                    <th>Sexe :</th>
-                                    <td>'.$report->patient->genre.' </td>
-                                    <th>Médecin prescripteur : </th>
-                                    <td>'.$report->order->doctor->name.' </td>
-                                </tr>
-                                <tr><td colspan=4></td></tr>
-                                <tr><td colspan=4></td></tr>
-                                <tr><td colspan=4></td></tr>
-                                <tr><td colspan=4></td></tr>
-                                <tr >
-                                    <th>Infos complémentaire *: </th>
-                                    <td >it maecenas tortor. Donec.</td>
-                                
-                                    <th>Commentaire *: </th>
-                                    <td >it maecenas tortor. Donec </td>
-                                </tr>
-                            </tbody>
-                       </table>
-                    </p>
-                    <br>
-                </fieldset>
-            </div>
-            <br><br>
-            <div >
-                <fieldset style="border: 3px solid rgb(0,0,0,0)">
-                    <legend style="font-size: 1.5em;padding-bottom: 10px; padding-left: 5px; padding-right: 5px;border:none; background-color:rgb(255,255,255);">
-                        <b>Récapitulatifs </b>
-                    </legend>
-                    <p style="margin-left:10px; margin-right:10px; display: inline-block; width: 75%;font-family:Courier; font-size:14px; ">
-                        <b>Examen / Type </b> : Cytoponction Mammaire
-                        <p style="text-justify; width: -moz-fit-content; width: fit-content;">
-                            '.htmlspecialchars_decode($report->description).'
-                        </p>
-                    </p>
-                    <br>
-                </fieldset>
-            </div>
-            <div style="">
-                <p style="text-align:right; margin-right:250px; font-size:20px; font-family:courier;"><img width="50" height="60" src="'.storage_path('app/public/'.$data['signature1']).'" alt=""> 
-                <br>
-                <b> <i style="font-size:10px; color: blue">'.$data['signatory1'].'
-                <br>
-                Spécialité</i></b>
-                </p>
-                <p style="text-align:right;margin-top:-70px; font-size:20px; font-family:courier;"><img width="50" height="60" src="'.storage_path('app/public/'.$data['signature2']).'" alt="">
-                    <br>
-                    <b> <i style="font-size:10px; color: blue">
-                    <br>
-                    Spécialité</i></b>
-                </p>
-            </div>
-            <br><br>
-            <div style="align-text:center;font-family:courier;font-size:12px;">
-                Centre ADECHINA Anatomie Pathologique • <br>
-                Adresse : XXXXXXXXXXXXXX • Téléphone : XXXXXXXX/XXXXXXXX • RCCM XXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                <br>
-                Contact@caap.bj • Ouvert du Lundi au Vendredi de 08:00 - 12:00 / 14:00 - 18:00 • www.caap.bj
-            </div>
-  
-        ');
-        $newname= 'CO-'.$report->order->code.'.pdf';
-        $html2pdf->output($newname);
     }
 }
--- resources/views/pdf/canva.blade.php
+++ resources/views/pdf/canva.blade.php
@@ -1,167 +1,130 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        @page {
-            margin: 0cm 0cm;
-        }
-
-        body {
-            margin-top: 2cm;
-            margin-left: 2cm;
-            margin-right: 2cm;
-            margin-bottom: 2cm;
-        }
-    </style>
-</head>
-
-<body>
-    <div style="display:inline-block; ">
-        <span style="display: inline-block;padding: 5px; font-size:25px; margin-top:-50px ">Logo</span>
-        <div
-            style="display: inline-block; padding: 5px; position: absolute; top:20px; right: 0px; padding: 10px; text-align:right;">
-            <b><span style="font-size:20px; text-align:right;"> CENTRE ADECHINA ANATOMIE PATHOLOGIE</span></b>
-            <br><span style="font-size:10px; text-align:right;">Laboratoire d’Anatomie Pathologique</span>
-        </div>
-    </div>
-    <div style="display: inline-block; position: absolute; right: 0px;width: 150px;padding: 10px;  margin-top:-20px">
-        <p>
-            <b>N° ANAPTH :</b> XXXXXX
-            <br>
-            <b>Date :</b> 14 Juillet 2022
-        </p>
-    </div>
+<div style="display:inline-block; ">
+    <span style="display: inline-block;padding-top: 5px; font-size:25px; margin-top:-50px "><img
+            src="{{ public_path('adminassets/images/Labo_Logo_1.png') }}" alt=""></span>
     <div
-        style="margin-top:20px; background-color:blue; width:100%; height:50px;color:rgb(255,255,255); text-align: center; padding-top:19px;font-size:25px;">
-        <b>COMPTE RENDU HISTOPATHOLOGIE</b>
+        style="display: inline-block; padding: 5px; position: absolute; top:20px; right: 0px; padding: 10px; text-align:right;">
+        <b><span style="font-size:17px; text-align:right;"> CENTRE ADECHINA ANATOMIE PATHOLOGIE</span></b>
+        <br><span style="font-size:10px; text-align:right;">Laboratoire d’Anatomie Pathologique</span>
     </div>
-    <br><br>
-    <div>
-        <fieldset style="border: 3px solid rgb(0,0,0,0)">
-            <legend style="font-size: 1.5em;padding: 5px;border:none; background-color:rgb(255,255,255);">
-                <b>Informations prélèvement</b>
-            </legend>
-            <p style="margin-left:10px; margin-right:10px; display:block; width: 100%;">
-
-
-            <table style="max-width: 100%;width: 500px ">
-                <tbody>
-                    <tr>
-                        <th>Nom :</th>
-                        <td>DOE </td>
-                        <th>Date prélèvement : </th>
-                        <td>12 Juillet 2022 </td>
-                    </tr>
-                    <tr>
-                        <th>Prénoms :</th>
-                        <td>John </td>
-                        <th>Date d’arrivée labo : </th>
-                        <td>13 Juillet 2022 </td>
-                    </tr>
-                    <tr>
-                        <th>Age :</th>
-                        <td>30 ans </td>
-                        <th>Service demandeur </th>
-                        <td>CNHU </td>
-                    </tr>
-                    <tr>
-                        <th>Sexe :</th>
-                        <td>Masculin (M) </td>
-                        <th>Médecin prescripteur : </th>
-                        <td>Dr. John DOE </td>
-                    </tr>
-                    <tr>
-                        <td colspan=4></td>
-                    </tr>
-                    <tr>
-                        <td colspan=4></td>
-                    </tr>
-                    <tr>
-                        <td colspan=4></td>
-                    </tr>
-                    <tr>
-                        <td colspan=4></td>
-                    </tr>
-                    <tr>
-                        <th>Infos complémentaire *: </th>
-                        <td>it maecenas tortor. Donec.</td>
-
-                        <th>Commentaire *: </th>
-                        <td>it maecenas tortor. Donec </td>
-                    </tr>
-                </tbody>
-            </table>
-            </p>
-            <br>
-        </fieldset>
-    </div>
-    <br><br>
-    <div>
-        <fieldset style="border: 3px solid rgb(0,0,0,0)">
-            <legend style="font-size: 1.5em;padding: 5px;border:none; background-color:rgb(255,255,255);">
-                <b>Récapitulatifs </b>
-            </legend>
-            <p
-                style="margin-left:10px; margin-right:10px; display:block; width: 100%;font-family:Courier; font-size:14px;">
-                <b>Examen / Type </b> : Cytoponction Mammaire
-            <p style="text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum tempor tempor massa,
-                tortor in vitae sed consectetur. Nisl egestas pharetra tristique commodo duis. Feugiat
-                congue dignissim nibh augue vitae pharetra tellus at diam. Porta a risus maecenas
-                suspendisse sit vulputate. Viverra suspendisse dui turpis tempor ligula augue suscipit dis
-                non.
-                <br>
-                <br>
-                Commodo lectus nisl a dui purus sociis non aenean dis. Rhoncus diam, lectus in vivamus in
-                in cursus rhoncus neque. Potenti turpis phasellus volutpat odio massa elementum neque.
-                Neque sit tellus morbi ac cursus feugiat. Facilisi aenean lacus cursus volutpat blandit lacus,
-                sed nunc. Est ut tincidunt etiam tempus elementum, ut commodo.
-                <br>
-                <br>
-                Gravida mi amet feugiat arcu erat risus. Eu sed varius hendrerit vulputate ut lectus risus.
-                Egestas morbi dui, blandit ornare tortor, pulvinar egestas aenean ut. Egestas donec rutrum
-                eu enim aliquet pellentesque sit maecenas tortor. Donec aliquam maecenas venenatis et
-                lacinia egestas phasellus consectetur. Egestas non sapien amet amet sapien duis sed.
-                <br>
-                <br>
-
-                <b>Conclusion :</b>
-            <div style="margin-left:10px;width:80%;text-align:justify"> Lorem ipsum dolor sit amet, consectetur
-                adipiscing
-                elit. Arcu nibh purus
-                penatibus non semper quis nulla justo, scelerisque. Hac nisl, elementum,
-                interdum mauris, urna enim. Ut massa auctor in egestas mauris vel libero.</div>
-            </p>
-            </p>
-            <br>
-        </fieldset>
-    </div>
-    <div style="margin-right:70px;">
-        <p style="text-align:right; margin-right:250px; font-size:20px; font-family:courier;">Signature
-            <br>
-            <b> <i style="font-size:10px; color: blue">Dr. Jane DOE
-                    <br>
-                    Spécialité</i></b>
-        </p>
-        <p style="text-align:right;margin-top:-70px; font-size:20px; font-family:courier;">Signature
-            <br>
-            <b> <i style="font-size:10px; color: blue">Dr. Jane DOE
-                    <br>
-                    Spécialité</i></b>
-        </p>
-    </div>
-    <br><br>
-    <div style="align-text:center;font-family:courier;font-size:7px;">
-        Centre ADECHINA Anatomie Pathologique • <br>
-        Adresse : XXXXXXXXXXXXXX • Téléphone : XXXXXXXX/XXXXXXXX • RCCM XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+</div>
+<div style="display: inline-block; position: absolute; right: 0px;width: 150px;padding: 10px;  margin-top:-20px">
+    <p>
+        <b>N° ANAPTH :</b> XXXXXX
         <br>
-        Contact@caap.bj • Ouvert du Lundi au Vendredi de 08:00 - 12:00 / 14:00 - 18:00 • www.caap.bj
-    </div>
-</body>
+        <b>Date :</b> 14 Juillet 2022
+    </p>
+</div>
+<div
+    style="margin-top:20px; background-color:#0070C1; width:100%; height:50px;color:rgb(255,255,255); text-align: center; padding-top:19px;font-size:25px;">
+    <b>COMPTE RENDU HISTOPATHOLOGIE</b>
+</div>
+<br><br>
+<div>
+    <fieldset style="border: 3px solid rgb(0,0,0,0)">
+        <legend
+            style="font-size: 1.5em; padding-bottom: 10px; padding-left: 5px; padding-right: 5px; border:none; background-color:rgb(255,255,255); ">
+            <b>Informations prélèvement</b>
+        </legend>
+        <p style="margin-left:10px; margin-right:10px; display:block; width: 100%;">
 
-</html>
+
+        <table style="max-width: 100%;width: 500px ">
+            <tbody>
+                <tr>
+                    <th>Nom :</th>
+                    <td>{{ $patient_firstname }} </td>
+                    <th>Date prélèvement : </th>
+                    <td>12 Juillet 2022 </td>
+                </tr>
+                <tr>
+                    <th>Prénoms :</th>
+                    <td>{{ $patient_lastname }} </td>
+                    <th>Date d’arrivée labo : </th>
+                    <td>13 Juillet 2022 </td>
+                </tr>
+                <tr>
+                    <th>Age :</th>
+                    <td>{{ $patient_age }} </td>
+                    <th>Service demandeur </th>
+                    <td>{{ $hospital_name }} </td>
+                </tr>
+                <tr>
+                    <th>Sexe :</th>
+                    <td>{{ $patient_genre }} </td>
+                    <th>Médecin prescripteur : </th>
+                    <td> {{ $doctor_name }} </td>
+                </tr>
+                <tr>
+                    <td colspan=4></td>
+                </tr>
+                <tr>
+                    <td colspan=4></td>
+                </tr>
+                <tr>
+                    <td colspan=4></td>
+                </tr>
+                <tr>
+                    <td colspan=4></td>
+                </tr>
+                <tr>
+                    <th>Infos complémentaire *: </th>
+                    <td>it maecenas tortor. Donec.</td>
+
+                    <th>Commentaire *: </th>
+                    <td>it maecenas tortor. Donec </td>
+                </tr>
+            </tbody>
+        </table>
+        </p>
+        <br>
+    </fieldset>
+</div>
+<br><br>
+<div>
+    <fieldset style="border: 3px solid rgb(0,0,0,0)">
+        <legend
+            style="font-size: 1.5em;padding-bottom: 10px; padding-left: 5px; padding-right: 5px;border:none; background-color:rgb(255,255,255);">
+            <b>Récapitulatifs </b>
+        </legend>
+        <p
+            style="margin-left:10px; margin-right:10px; display: inline-block; width: 75%;font-family:Courier; font-size:14px; ">
+            <b>Examen / Type </b> : Cytoponction Mammaire
+        <p style="text-justify; width: -moz-fit-content; width: fit-content;">
+            {!! $content !!}
+        </p>
+        </p>
+        <br>
+    </fieldset>
+</div>
+<div style="">
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align: left;    width: 25%"></td>
+            <td style="text-align: left;    width: 25%"></td>
+            <td style="text-align: right;    width: 25%"><img width="50" height="60"
+                    src="{{ storage_path('app/public/' . $signature1) }}" alt=""><br> <i
+                    style="font-size:10px; color: blue">{{ $signatory1 }}
+                    <br>
+                    Spécialité</i></td>
+            <td style="text-align: right;    width: 25%"><img width="50" height="60"
+                    src="{{ storage_path('app/public/' . $signature2) }}" alt=""><br> <i
+                    style="font-size:10px; color: blue">{{ $signatory2 }}
+                    <br>
+                    Spécialité</i></td>
+        </tr>
+    </table>
+
+</div>
+<br><br>
+
+<page_footer>
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align: left;    width: 100%"> Centre ADECHINA Anatomie Pathologique • <br>
+                Adresse : XXXXXXXXXXXXXX • Téléphone : XXXXXXXX/XXXXXXXX • RCCM XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                <br>
+                Contact@caap.bj • Ouvert du Lundi au Vendredi de 08:00 - 12:00 / 14:00 - 18:00 • www.caap.bj
+            </td>
+        </tr>
+    </table>
+</page_footer>
