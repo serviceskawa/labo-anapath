9ab4c54b9f2487010a75caa4916d767c50e7a75e
kawa <serviceskawa@gmail.com>
2022-09-26 22:42:47 +0100

    config envoi email update

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => 'mail.caap.bj',
+            'port' => 26,
+            'encryption' => 'tls',
+            'username' => 'admin@caap.bj',
+            'password' => 'h&qa4D@l7J',
             'timeout' => null,
             'auth_mode' => null,
         ],
