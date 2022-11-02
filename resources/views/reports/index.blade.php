bb7f02232ae3f8b31ab08ad7fdc3847ae11275ff
Thuret <tgenougan@gmail.com>
2022-11-02 13:31:07 +0100

    reports search

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -3,94 +3,94 @@
 @section('title', 'Reports')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <h4 class="page-title">Comptes rendu</h4>
-
-            </div>
-
-            <!----MODAL---->
-
-            @include('contrats.create')
-
-            @include('contrats.edit')
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            <h4 class="page-title">Comptes rendu</h4>
 
         </div>
+
+        <!----MODAL---->
+
+        @include('contrats.create')
+
+        @include('contrats.edit')
+
     </div>
+</div>
 
 
-    <div class="">
+<div class="">
 
 
-        @include('layouts.alerts')
+    @include('layouts.alerts')
 
 
 
-        <div class="card mb-md-0 mb-3">
-            <div class="card-body">
-                <div class="card-widgets">
-                    <a href="javascript:;" data-bs-toggle="reload"><i class="mdi mdi-refresh"></i></a>
-                    <a data-bs-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false"
-                        aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
-                    <a href="#" data-bs-toggle="remove"><i class="mdi mdi-close"></i></a>
-                </div>
-                <h5 class="card-title mb-0">Liste des comptes rendu</h5>
-
-                <div id="cardCollpase1" class="collapse pt-3 show">
-
-
-                    <table id="datatable1" class="table table-striped dt-responsive nowrap w-100">
-                        <thead>
-                            <tr>
-                                <th>Code</th>
-                                <th>Code patient</th>
-                                <th>Nom Patient</th>
-                                <th>Telephone</th>
-                                <th>Date</th>
-                                <th>Statut</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-
-
-                        <tbody>
-
-                            @foreach ($reports as $item)
-                                <tr>
-                                    <td>{{ $item->order->code }}</td>
-                                    <td>{{ $item->patient->code }}</td>
-                                    <td>{{ $item->patient->firstname }} {{ $item->patient->lastname }}</td>
-                                    <td>{{ $item->patient->telephone1 }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($item->created_at)->format('d/m/Y') }}</td>
-                                    <td>{{ $item->status == '1' ? 'Valider' : 'Attente' }}</td>
-
-                                    <td>
-                                        <a type="button" href="{{ route('report.show', $item->id) }}"
-                                            class="btn btn-primary"><i class="mdi mdi-eye"></i> </a>
-                                        <a type="button" href="{{ route('report.send-sms', $item->id) }}"
-                                            class="btn btn-danger"><i class="mdi mdi-android-messages"></i> </a>
-                                        <a type="button" href="{{ route('report.pdf', $item->id) }}"
-                                            class="btn btn-secondary"><i class="mdi mdi-printer"></i> </a>
-                                    </td>
-
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-                </div>
+    <div class="card mb-md-0 mb-3">
+        <div class="card-body">
+            <div class="card-widgets">
+                <a href="javascript:;" data-bs-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                <a data-bs-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false"
+                    aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
+                <a href="#" data-bs-toggle="remove"><i class="mdi mdi-close"></i></a>
             </div>
-        </div> <!-- end card-->
+            <h5 class="card-title mb-0">Liste des comptes rendu</h5>
+
+            <div id="cardCollpase1" class="collapse pt-3 show">
 
 
-    </div>
+                <table id="datatable1" class="table table-striped dt-responsive nowrap w-100">
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>Code patient</th>
+                            <th>Nom Patient</th>
+                            <th>Telephone</th>
+                            <th>Date</th>
+                            <th>Statut</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+
+                        @foreach ($reports as $item)
+                        <tr>
+                            <td>{{ $item->order->code }}</td>
+                            <td>{{ $item->patient->code }}</td>
+                            <td>{{ $item->patient->firstname }} {{ $item->patient->lastname }}</td>
+                            <td>{{ $item->patient->telephone1 }}</td>
+                            <td>{{ \Carbon\Carbon::parse($item->created_at)->format('d/m/Y') }}</td>
+                            <td>{{ $item->status == '1' ? 'Valider' : 'Attente' }}</td>
+
+                            <td>
+                                <a type="button" href="{{ route('report.show', $item->id) }}" class="btn btn-primary"><i
+                                        class="mdi mdi-eye"></i> </a>
+                                <a type="button" href="{{ route('report.send-sms', $item->id) }}"
+                                    class="btn btn-danger"><i class="mdi mdi-android-messages"></i> </a>
+                                <a type="button" href="{{ route('report.pdf', $item->id) }}"
+                                    class="btn btn-secondary"><i class="mdi mdi-printer"></i> </a>
+                            </td>
+
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div> <!-- end card-->
+
+
+</div>
 @endsection
 
 
 @push('extra-js')
-    <script>
-        // SUPPRESSION
+<script>
+    // SUPPRESSION
         function deleteModal(id) {
 
             Swal.fire({
@@ -121,7 +121,7 @@
                 ],
                 "columnDefs": [{
                     "targets": [0],
-                    "searchable": false
+                    "searchable": true
                 }],
                 "language": {
                     "lengthMenu": "Afficher _MENU_ enregistrements par page",
@@ -166,5 +166,5 @@
                 }
             });
         }
-    </script>
+</script>
 @endpush
