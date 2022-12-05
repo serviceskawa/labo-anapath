516e680bb2f4caad9c3dca790407f7493c96e2aa
Thuret <tgenougan@gmail.com>
2022-12-05 14:32:36 +0100

    filtre de la liste de demande d'examen debuté

--- resources/views/examens/index.blade.php
+++ resources/views/examens/index.blade.php
@@ -3,11 +3,12 @@
 @section('title', 'Examens')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                {{-- <div class="page-title-right mr-3">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#standard-modal">Nouveau</button>
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            {{-- <div class="page-title-right mr-3">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#standard-modal">Nouveau</button>
             </div>
             <h4 class="page-title">Gérer les demandes d'examen</h4>
         </div>
@@ -16,97 +17,150 @@
 
         {{-- @include('examens.create') --}}
 
-                {{-- @include('doctors.edit') --}}
+        {{-- @include('doctors.edit') --}}
+
+    </div>
+</div>
+
+
+<div class="">
+
+    <div class="page-title-box">
+        <div class="page-title-right mr-3">
+            <a href="{{ route('test_order.create') }}"><button type="button" class="btn btn-primary">Ajouter une
+                    nouvelle demande d'examen</button></a>
+        </div>
+        <h4 class="page-title">Demandes d'examen</h4>
+    </div>
+
+    <!----MODAL---->
+    @include('layouts.alerts')
+
+    <div class="card mb-md-0 mb-3">
+        <div class="card-body">
+            <div class="card-widgets">
+                <a href="javascript:;" data-bs-toggle="reload"><i class="mdi mdi-refresh"></i></a>
+                <a data-bs-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false"
+                    aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
+                <a href="#" data-bs-toggle="remove"><i class="mdi mdi-close"></i></a>
+            </div>
+            <h5 class="card-title mb-0">Liste des demandes d'examen </h5>
+
+            <div id="cardCollpase1" class="collapse pt-3 show">
+
+                <form action="" method="post">
+                    <div class="row mb-3">
+
+                        <div class="col-lg-3">
+
+                            <div class="mb-3">
+                                <label for="example-fileinput" class="form-label">Date</label>
+                                <select name="date" class="form-control" id="">
+                                    <option value="today">Aujourd'hui</option>
+                                </select>
+                            </div>
+
+
+                        </div> <!-- end col -->
+
+                        <div class="col-lg-3">
+
+                            <div class="mb-3">
+                                <label for="example-fileinput" class="form-label">Contrat</label>
+                                <select name="contrat_id" class="form-control" id="">
+                                    <option value="all_contrat">Tous les contrats</option>
+                                </select>
+                            </div>
+
+
+                        </div> <!-- end col -->
+
+                        <div class="col-lg-3">
+
+                            <div class="mb-3">
+                                <label for="example-fileinput" class="form-label">Status</label>
+                                <select name="status" class="form-control" id="">
+                                    <option value="all_status">Tous les status</option>
+                                </select>
+                            </div>
+
+
+                        </div> <!-- end col -->
+
+                        <div class="col-lg-3">
+
+                            <div class="mb-3">
+                                <label for="example-fileinput" class="form-label">Urgent</label>
+                                <select name="cas_status" class="form-control" id="">
+                                    <option value="all_status">Tous les status</option>
+                                </select>
+                            </div>
+
+
+                        </div> <!-- end col -->
+
+                    </div>
+                </form>
+
+                <table id="datatable1" class="table table-striped dt-responsive nowrap w-100">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Date</th>
+                            <th>Code</th>
+                            <th>Patient</th>
+                            <th>Examens demandés</th>
+                            <th>Montant</th>
+                            <th>Compte rendu</th>
+                            <th>Actions</th>
+
+                        </tr>
+                    </thead>
+
+
+                    <tbody>
+
+                        @foreach ($examens as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            {{-- <td>{{ \Carbon\Carbon::parse($item->created_at)->format('d/m/Y') }}</td> --}}
+                            <td>{{ $item->code }} </td>
+                            <td>{{ $item->patient->firstname }} {{ $item->patient->lastname }}</td>
+                            <td>{{ $item->getDoctor()->name }}</td>
+                            <td>{{ $item->getHospital()->name }}</td>
+                            <td>{{ $item->total }}</td>
+                            <td>
+                                <a type="button" href="{{ route('details_test_order.index', $item->id) }}"
+                                    class="btn btn-primary"><i class="mdi mdi-eye"></i> </a>
+                                @if ($item->status != 1)
+                                <button type="button" onclick="deleteModal({{ $item->id }})" class="btn btn-danger"><i
+                                        class="mdi mdi-trash-can-outline"></i>
+                                </button>
+                                @endif
+
+                            </td>
+
+                        </tr>
+                        @endforeach
+
+
+
+
+                    </tbody>
+                </table>
 
             </div>
         </div>
+    </div> <!-- end card-->
 
 
-        <div class="">
-
-            <div class="page-title-box">
-                <div class="page-title-right mr-3">
-                    <a href="{{ route('test_order.create') }}"><button type="button" class="btn btn-primary">Ajouter une
-                            nouvelle demande d'examen</button></a>
-                </div>
-                <h4 class="page-title">Demandes d'examen</h4>
-            </div>
-
-            <!----MODAL---->
-            @include('layouts.alerts')
-
-            <div class="card mb-md-0 mb-3">
-                <div class="card-body">
-                    <div class="card-widgets">
-                        <a href="javascript:;" data-bs-toggle="reload"><i class="mdi mdi-refresh"></i></a>
-                        <a data-bs-toggle="collapse" href="#cardCollpase1" role="button" aria-expanded="false"
-                            aria-controls="cardCollpase1"><i class="mdi mdi-minus"></i></a>
-                        <a href="#" data-bs-toggle="remove"><i class="mdi mdi-close"></i></a>
-                    </div>
-                    <h5 class="card-title mb-0">Liste des demandes d'examen </h5>
-
-                    <div id="cardCollpase1" class="collapse pt-3 show">
+</div>
+@endsection
 
 
-                        <table id="datatable1" class="table table-striped dt-responsive nowrap w-100">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Date</th>
-                                    <th>Code</th>
-                                    <th>Patient</th>
-                                    <th>Médecin</th>
-                                    <th>Hopital</th>
-                                    <th>Montant</th>
-                                    <th>Actions</th>
-
-                                </tr>
-                            </thead>
-
-
-                            <tbody>
-
-                                @foreach ($examens as $item)
-                                    <tr>
-                                        <td>{{ $item->id }}</td>
-                                        <td>{{ \Carbon\Carbon::parse($item->created_at)->format('d/m/Y') }}</td>
-                                        <td>{{ $item->code }} </td>
-                                        <td>{{ $item->patient->firstname }} {{ $item->patient->lastname }}</td>
-                                        <td>{{ $item->getDoctor()->name }}</td>
-                                        <td>{{ $item->getHospital()->name }}</td>
-                                        <td>{{ $item->total }}</td>
-                                        <td>
-                                            <a type="button" href="{{ route('details_test_order.index', $item->id) }}"
-                                                class="btn btn-primary"><i class="mdi mdi-eye"></i> </a>
-                                            @if ($item->status != 1)
-                                                <button type="button" onclick="deleteModal({{ $item->id }})"
-                                                    class="btn btn-danger"><i class="mdi mdi-trash-can-outline"></i>
-                                                </button>
-                                            @endif
-
-                                        </td>
-
-                                    </tr>
-                                @endforeach
-
-
-
-
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
-            </div> <!-- end card-->
-
-
-        </div>
-    @endsection
-
-
-    @push('extra-js')
-        <script>
-            // SUPPRESSION
+@push('extra-js')
+<script>
+    // SUPPRESSION
             function deleteModal(id) {
 
                 Swal.fire({
@@ -195,5 +249,5 @@
                     }
                 });
             }
-        </script>
-    @endpush
+</script>
+@endpush
