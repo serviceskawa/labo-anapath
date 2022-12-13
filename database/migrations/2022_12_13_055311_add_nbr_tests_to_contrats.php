8772c449ac40eee9afbc9ea628f8549459c0d647
Thuret <tgenougan@gmail.com>
2022-12-13 08:47:16 +0100

    migration

---
+++ database/migrations/2022_12_13_055311_add_nbr_tests_to_contrats.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNbrTestsToContrats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contrats', function (Blueprint $table) {
+            $table->integer('nbr_tests')->nullable()->default(-1);
+            $table->binary('is_close')->nullable()->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contrats', function (Blueprint $table) {
+            //
+        });
+    }
+}
