764bff68e7e0da5513799d4098679e08915bae96
kawa <serviceskawa@gmail.com>
2023-01-25 21:06:08 +0100

    update report template

--- resources/views/pdf/canva.blade.php
+++ resources/views/pdf/canva.blade.php
@@ -9,7 +9,7 @@ date_default_timezone_set('Europe/Paris');
             src="{{ public_path('adminassets/images/Logo_long_CAAP@4x.png') }}" width="200px;" alt=""></span>
     <div
         style="display: inline-block; padding: 5px; position: absolute; top:20px; right: 0px; padding: 10px; text-align:right;">
-        <b><span style="font-size:17px; text-align:right;"> CENTRE ADECHINA ANATOMIE PATHOLOGIE</span></b>
+        <b><span style="font-size:17px; text-align:right;"> CENTRE ADECHINA ANATOMIE PATHOLOGIQUE</span></b>
         <br><span style="font-size:10px; text-align:right;">Laboratoire d’Anatomie Pathologique</span>
     </div>
 </div>
@@ -38,13 +38,13 @@ date_default_timezone_set('Europe/Paris');
             <tbody>
                 <tr>
                     <th width="25%;">Nom :</th>
-                    <td width="25%;">{{ $patient_lastname }} </td>
+                    <td width="25%;">{{ $patient_firstname }} </td>
                     <th width="40%;">Date prélèvement : </th>
                     <td width="20%;">{{$prelevement_date}} </td>
                 </tr>
                 <tr>
                     <th>Prénoms :</th>
-                    <td>{{ $patient_firstname }} </td>
+                    <td>{{ $patient_lastname }} </td>
                     <th>Date d’arrivée labo : </th>
                     <td> {{ $created_at }} </td>
                 </tr>
@@ -78,10 +78,10 @@ date_default_timezone_set('Europe/Paris');
         <p
             style="text-left; width: -moz-fit-content; width: fit-content; margin-left:10px; margin-right:10px; font-familly:Montserrat;">
 
-        <table style="max-width: 100%;width: 500px ">
+        <table style="table-layout: fixed;max-width: 100%;width: 500px ">
             <tbody>
                 <tr>
-                    {!! $content !!}
+                    <td> {!! $content !!} </td>
                 </tr>
             </tbody>
         </table>
@@ -94,24 +94,24 @@ date_default_timezone_set('Europe/Paris');
 <div style="">
     <table style="width: 100%;">
         <tr>
-            <td style="text-align: left;    width: 25%"></td>
+            <!-- <td style="text-align: left;    width: 10%"></td> -->
 
-            <td style="text-align: right;    width: 25%">
+            <td style="text-align: center;    width: 33%">
                 @if ($signatory1 != null)
-                <img width="50" height="60" src="{{ storage_path('app/public/' . $signature1) }}" alt=""><br><br>{{
+                <img width="100" src="{{ storage_path('app/public/' . $signature1) }}" alt=""><br><br>{{
                 $signatory1 }}
                 @endif
             </td>
-            <td style="text-align: right;    width: 25%">
+            <td style="text-align: center;    width: 33%">
                 @if ($signatory2 != null)
-                <img width="50" height="60" src="{{ storage_path('app/public/' . $signature2) }}" alt=""><br><br>{{
+                <img width="200" src="{{ storage_path('app/public/' . $signature2) }}" alt=""><br><br>{{
                 $signatory2 }}
                 @endif
 
             </td>
-            <td style="text-align: right;    width: 25%">
+            <td style="text-align: center;    width: 33%">
                 @if ($signatory3 != null)
-                <img width="50" height="60" src="{{ storage_path('app/public/' . $signature3) }}" alt=""><br><br>{{
+                <img width="85" src="{{ storage_path('app/public/' . $signature3) }}" alt=""><br><br>{{
                 $signatory3 }}
                 @endif
             </td>
@@ -127,7 +127,7 @@ date_default_timezone_set('Europe/Paris');
             <td style="text-align: left; width: 100%; font-size:12px;"> Centre ADECHINA Anatomie Pathologique • <br>
                 Adresse : Carre 1915 "G" Fifadji, 072 BP 059 Cotonou, Benin • Téléphone : (+229)96110311 • RCCM
                 RB/COT/18 B22364<br>
-                Contact@caap.bj • Ouvert du Lundi au Vendredi de 08:00 - 12:00 / 14:00 - 18:00 • www.caap.bj
+                Contact@caap.bj • Ouvert du Lundi au Vendredi de 08:00 - 17:00 • www.caap.bj
             </td>
         </tr>
     </table>
