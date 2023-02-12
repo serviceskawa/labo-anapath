7393c9e24d4ed65514d638a3832c5ac0aaba383f
Thuret <tgenougan@gmail.com>
2023-02-12 07:40:57 +0100

    fusion view and edit doned

--- resources/views/examens/details/index.blade.php
+++ resources/views/examens/details/index.blade.php
@@ -40,9 +40,58 @@
                 <div class="card-body">
 
                     @csrf
-                    <div class="row mb-3">
+                    <div style="text-align:right;"><span style="color:red;">*</span>champs obligatoires</div>
 
-                        <div style="text-align:right;"><span style="color:red;">*</span>champs obligatoires</div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="exampleFormControlInput1" class="form-label">Type d'examen<span
+                                    style="color:red;">*</span></label>
+                            <select class="form-select select2" data-toggle="select2" required id="type_examen"
+                                name="type_examen">
+                                <option>Sélectionner le type d'examen</option>
+                                @forelse ($types_orders as $type)
+                                    <option {{ $test_order->type_order_id == $type->id ? 'selected' : '' }}
+                                        value="{{ $type->id }}">{{ $type->title }}</option>
+                                        
+                                @empty
+                                    Ajouter un Type d'examen
+                                @endforelse
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="exampleFormControlInput1" class="form-label">Contrat<span
+                                    style="color:red;">*</span></label>
+                            <select class="form-select select2" data-toggle="select2" required name="contrat_id">
+                                <option>Sélectionner le contrat</option>
+                                @forelse ($contrats as $contrat)
+                                    <option value="{{ $contrat->id }}"
+                                        {{ $test_order->contrat_id == $contrat->id ? 'selected' : '' }}>
+                                        {{ $contrat->name }}</option>
+                                @empty
+                                    Ajouter un contrat
+                                @endforelse
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12 my-3">
+
+                        <div class="examenReferenceSelect" style="display: none !important">
+                            <label for="exampleFormControlInput1" class="form-label">Examen de Référence<span
+                                    style="color:red;">*</span></label>
+                            <select class="form-select select2" data-toggle="select2" name="examen_reference_select"
+                                id="examen_reference_select">
+                                <option value="">Sélectionner dans la liste</option>
+                            </select>
+                        </div>
+                        <div class="examenReferenceInput" style="display: none !important">
+                            <label for="exampleFormControlInput1" class="form-label">Examen de Référence<span
+                                    style="color:red;">*</span></label>
+                            <input type="text" name="examen_reference_input" class="form-control"
+                                placeholder="Saisir l'examen de reference" value="{{ $test_order->test_affiliate }}">
+                        </div>
+
+                    </div>
+                    <div class="row mb-3">
 
                         <div class="col-md-6">
                             <label for="exampleFormControlInput1" class="form-label">Patient <span
@@ -105,76 +154,8 @@
                             <label class="form-label">Date prélèvement<span style="color:red;">*</span></label>
                             <input type="date" class="form-control" name="prelevement_date" id="prelevement_date"
                                 data-date-format="dd/mm/yyyy" value="{{ $test_order->prelevement_date }}" required>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label for="example-fileinput" class="form-label">Pièce jointe</label>
-                                <input type="file" name="examen_file" id="example-fileinput" class="form-control">
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <label for="exampleFormControlInput1" class="form-label">Type d'examen<span
-                                    style="color:red;">*</span></label>
-                            <select class="form-select select2" data-toggle="select2" required id="type_examen"
-                                name="type_examen">
-                                <option>Sélectionner le type d'examen</option>
-                                @forelse ($types_orders as $type)
-                                    <option value="{{ $type->id }}"
-                                        {{ $test_order->type_order_id == $type->id ? 'selected' : '' }}>
-                                        {{ $type->title }}</option>
-                                @empty
-                                    Ajouter un Type d'examen
-                                @endforelse
-                            </select>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="exampleFormControlInput1" class="form-label">Contrat<span
-                                    style="color:red;">*</span></label>
-                            <select class="form-select select2" data-toggle="select2" required name="contrat_id">
-                                <option>Sélectionner le contrat</option>
-                                @forelse ($contrats as $contrat)
-                                    <option value="{{ $contrat->id }}"
-                                        {{ $test_order->contrat_id == $contrat->id ? 'selected' : '' }}>
-                                        {{ $contrat->name }}</option>
-                                @empty
-                                    Ajouter un contrat
-                                @endforelse
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-md-12 mb-3">
-
-                        <div class="examenReferenceSelect" style="display: none !important">
-                            <label for="exampleFormControlInput1" class="form-label">Examen de Référence<span
-                                    style="color:red;">*</span></label>
-                            <select class="form-select select2" data-toggle="select2" name="examen_reference_select"
-                                id="examen_reference_select">
-                                <option value="">Sélectionner dans la liste</option>
-                            </select>
-                        </div>
-                        <div class="examenReferenceInput" style="display: none !important">
-                            <label for="exampleFormControlInput1" class="form-label">Examen de Référence<span
-                                    style="color:red;">*</span></label>
-                            <input type="text" name="examen_reference_input" class="form-control"
-                                placeholder="Saisir l'examen de reference" value="{{ $test_order->test_affiliate }}">
-                        </div>
-
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            {{-- <input type="checkbox" class="form-check-input" name="is_urgent" id=""
-                                {{ $test_order->is_urgent != 0 ? 'checked' : '' }}> --}}
-                            <label class="form-label">Cas urgent</label> <br>
-                            <input type="checkbox" id="switch3" class="form-control" name="is_urgent"
-                                {{ $test_order->is_urgent != 0 ? 'checked' : '' }} data-switch="success" />
-                            <label for="switch3" data-on-label="Urgent" data-off-label="Normal"></label>
-
-                        </div>
-                        <div class="col-md-6">
-                            <label class="form-label">Docteur signataire</label>
+                            <label class="form-label mt-3">Docteur signataire</label>
                             <select name="attribuate_doctor_id" id="" class="form-control">
                                 <option value="">Choississez un docteur signataire</option>
                                 @foreach (getUsersByRole('docteur') as $item)
@@ -183,9 +164,21 @@
                                         {{ $item->lastname }} {{ $item->firstname }} </option>
                                 @endforeach
                             </select>
+                            <label class="form-label mt-3">Cas urgent</label> <br>
+                            <input type="checkbox" id="switch3" class="form-control" name="is_urgent"
+                                {{ $test_order->is_urgent != 0 ? 'checked' : '' }} data-switch="success" />
+                            <label for="switch3" data-on-label="Urgent" data-off-label="Normal"></label>
 
                         </div>
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <label for="example-fileinput" class="form-label">Pièce jointe</label>
+                                <input type="file" name="examen_file" id="example-fileinput" class="form-control dropify" data-default-file="{{ $test_order ? Storage::url($test_order->examen_file) : '' }}">
+                            </div>
+                        </div>
                     </div>
+
+                    
 
                 </div>
 
@@ -646,4 +639,201 @@
     </script>
 
     {{-- Fusion --}}
+    <script>
+        $('.datepicker').datepicker({
+            format: 'dd/mm/yyyy',
+            startDate: '-3d'
+        });
+        // SUPPRESSION
+        function deleteModal(id) {
+
+            Swal.fire({
+                title: "Voulez-vous supprimer l'élément ?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Oui ",
+                cancelButtonText: "Non !",
+            }).then(function(result) {
+                if (result.value) {
+                    window.location.href = "{{ url('contrats_details/delete') }}" + "/" + id;
+                    Swal.fire(
+                        "Suppression !",
+                        "En cours de traitement ...",
+                        "success"
+                    )
+                }
+            });
+        }
+
+        //EDITION
+        function edit(id) {
+            var e_id = id;
+
+            // Populate Data in Edit Modal Form
+            $.ajax({
+                type: "GET",
+                url: "{{ url('getcontratdetails') }}" + '/' + e_id,
+                success: function(data) {
+
+                    $('#category_test_id2').val(data.category_test_id).change();
+                    $('#pourcentage2').val(data.pourcentage);
+                    $('#contrat_id2').val(data.contrat_id);
+                    $('#contrat_details_id2').val(data.id);
+
+
+
+                    console.log(data);
+                    $('#editModal').modal('show');
+                },
+                error: function(data) {
+                    console.log('Error:', data);
+                }
+            });
+        }
+
+        $(document).ready(function() {
+
+            $('#doctor_id').select2({
+                placeholder: 'Select Category',
+                theme: 'bootstrap4',
+                tags: true,
+            }).on('select2:close', function() {
+                var element = $(this);
+                var new_category = $.trim(element.val());
+
+                if (new_category != '') {
+                    $.ajax({
+                        url: "{{ route('doctors.storeDoctor') }}",
+                        method: "POST",
+                        data: {
+                            "_token": "{{ csrf_token() }}",
+                            name: new_category
+                        },
+                        success: function(data) {
+
+                            if (data.status === "created") {
+                                toastr.success("Donnée ajoutée avec succès", 'Ajout réussi');
+
+                                element.append('<option value="' + data.id + '">' + data.name +
+                                    '</option>').val(new_category);
+                            }
+                        }
+                    })
+                }
+
+            });
+
+            // Create hopital
+            $('#hospital_id').select2({
+                placeholder: 'Choisissez un hopital',
+                theme: 'bootstrap4',
+                tags: true,
+            }).on('select2:close', function() {
+                var element = $(this);
+                var new_category = $.trim(element.val());
+
+                if (new_category != '') {
+                    $.ajax({
+                        url: "{{ route('hopitals.storeHospital') }}",
+                        method: "POST",
+                        data: {
+                            "_token": "{{ csrf_token() }}",
+                            name: new_category
+                        },
+                        success: function(data) {
+
+                            if (data.status === "created") {
+                                toastr.success("Donnée ajoutée avec succès", 'Ajout réussi');
+
+                                element.append('<option value="' + data.id + '">' + data.name +
+                                    '</option>').val(new_category);
+                            }
+                        }
+                    })
+                }
+
+            });
+
+            $('#createPatientForm').on('submit', function(e) {
+                e.preventDefault();
+                let code = $('#code').val();
+                let lastname = $('#lastname').val();
+                let firstname = $('#firstname').val();
+                let age = $('#age').val();
+                let telephone1 = $('#telephone1').val();
+                let genre = $('#genre').val();
+                // alert(firstname);
+                $.ajax({
+                    url: "{{ route('patients.storePatient') }}",
+                    type: "POST",
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        code: code,
+                        lastname: lastname,
+                        firstname: firstname,
+                        age: age,
+                        telephone1: telephone1,
+                        genre: genre
+                    },
+                    success: function(data) {
+
+                        $('#createPatientForm').trigger("reset")
+                        $('#standard-modal').modal('hide');
+                        toastr.success("Donnée ajoutée avec succès", 'Ajout réussi');
+                        $('#patient_id').append('<option value="' + data.id + '">' + data.code +
+                                ' - ' + data.firstname + ' ' + data.lastname + '</option>')
+                            .trigger('change').val(data.id);
+
+                    },
+                    error: function(data) {
+                        console.log(data)
+                    },
+                    // processData: false,
+                });
+
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+            $('#type_examen').on('change', function(e) {
+                var typeExamenOption = $('#type_examen option:selected').text();
+                alert(typeExamenOption);
+                if (typeExamenOption == "Immuno Externe") {
+                    $(".examenReferenceSelect").hide();
+                    $(".examenReferenceInput").show();
+
+                } else if (typeExamenOption == "Immuno Interne") {
+                    $(".examenReferenceSelect").hide();
+                    $(".examenReferenceInput").show();
+
+                } else {
+                    $(".examenReferenceInput").hide();
+                    $(".examenReferenceSelect").hide();
+                }
+            });
+            
+
+        });
+
+        window.addEventListener("load", (event) => {
+            var typeExamenOption = $('#type_examen option:selected').text();
+                // alert(typeExamenOption);
+                if (typeExamenOption == "Immuno Externe") {
+                    $(".examenReferenceSelect").hide();
+                    $(".examenReferenceInput").show();
+
+                } else if (typeExamenOption == "Immuno Interne") {
+                    $(".examenReferenceSelect").hide();
+                    $(".examenReferenceInput").show();
+
+                } else {
+                    $(".examenReferenceInput").hide();
+                    $(".examenReferenceSelect").hide();
+                }
+
+        });
+    </script>
 @endpush
