d46a3e3f190504582fe3c833f14f638474702320
adetek-admin <benito@wallyskak.com>
2023-03-06 18:31:01 +0100

    report files add

---
+++ resources/views/settings/report/create.blade.php
@@ -0,0 +1,28 @@
+<div id="standard-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="standard-modalLabel">Ajouter un nouveau titre</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
+            </div>
+            <form action="{{ route('report.report-store') }}" method="POST" autocomplete="off">
+                @csrf
+                <div class="modal-body">
+                    <div style="text-align:right;"><span style="color:red;">*</span>champs obligatoires</div>
+                    
+                    <div class="mb-3">
+                        <label for="simpleinput" class="form-label">Titre<span
+                                style="color:red;">*</span></label>
+                        <input type="text" name="title" class="form-control" required>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Annuler</button>
+                    <button type="submit" class="btn btn-primary">Ajouter un nouveau titre</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
