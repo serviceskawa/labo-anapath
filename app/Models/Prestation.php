3217dab3189098e013f6aef51965d8470cb158ec
adetek-admin <benito@wallyskak.com>
2023-03-16 12:50:03 +0100

    dossier patient caap

--- app/Models/Prestation.php
+++ app/Models/Prestation.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use App\Models\Consultation;
 use App\Models\CategoryPrestation;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Prestation extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $guarded = [];
 
