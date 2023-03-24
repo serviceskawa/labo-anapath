e58b7b2d2aafa4e28a5e65ac807010f6ed560a09
adetek-admin <benito@wallyskak.com>
2023-03-24 14:52:35 +0100

    test order caap

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -226,8 +226,8 @@ class ReportController extends Controller
             'patient_age' => $report->patient->age,
             'patient_year_or_month' => $year_month,
             'patient_genre' => $report->patient->genre,
-            'hospital_name' => $report->order->hospital->name,
-            'doctor_name' => $report->order->doctor->name,
+            'hospital_name' => $report->order ? $report->order->hospital->name :'',
+            'doctor_name' => $report->order ? $report->order->doctor->name :'',
             'created_at' => date_format($report->created_at, "d/m/Y"),
             'date' => date('d/m/Y')
         ];
