f84ce15c5dc9785d5b26788285fb919de437fca0
DevZkc20 <cyrille@wallyskak.com>
2024-03-25 16:12:06 +0100

    Correction des filtres sur les factures

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -397,7 +397,7 @@ class InvoiceController extends Controller
                         ->orwhereHas('patient', function ($query) use ($request){
                             $query->where('firstname','like', '%'.$request->get('contenu').'%')
                             ->orwhere('lastname', 'like', '%'.$request->get('contenu').'%');
-                    });
+                    })->where('client_name','like', '%'.$request->get('contenu').'%');
                 }
 
 
