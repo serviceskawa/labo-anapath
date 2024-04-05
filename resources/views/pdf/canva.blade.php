f8b0779ff5f13ebb7fc696599a10d89bd537e4cc
DevZkc20 <cyrille@wallyskak.com>
2024-04-05 10:50:48 +0100

    Setting update

--- resources/views/pdf/canva.blade.php
+++ resources/views/pdf/canva.blade.php
@@ -20,7 +20,7 @@ date_default_timezone_set('Europe/Paris');
 
     <div style="display:inline-block; ">
         <span style="display: inline-block;padding-top: 5px; font-size:25px; margin-top:-50px "><img
-                src="{{ Storage::url($prefixe_code_demande_examen) }}" width="200px;" alt=""></span>
+                src="{{ Storage::url($entete) }}" width="200px;" alt=""></span>
         
     </div>
     <div
