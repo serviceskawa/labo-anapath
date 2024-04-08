ce0cfe1df51e73abd22e6f9a5b98bb6f7df9f66a
DevZkc20 <cyrille@wallyskak.com>
2024-04-08 09:45:17 +0100

    MIse a jour

--- resources/views/reports/assignment/pdf.blade.php
+++ resources/views/reports/assignment/pdf.blade.php
@@ -21,11 +21,6 @@ date_default_timezone_set('Europe/Paris');
     <div style="display:inline-block; ">
         <span style="display: inline-block;padding-top: 5px; font-size:25px; margin-top:-50px "><img
                 src="{{ Storage::url($entete) }}" width="200px;" alt=""></span>
-        <!--{{-- <div-->
-        <!--    style="display: inline-block; padding: 5px; position: absolute; top:20px; right: 0px; padding: 10px; text-align:right;">-->
-        <!--    <b><span style="font-size:17px; text-align:right;"> CENTRE ADECHINA ANATOMIE PATHOLOGIQUE</span></b>-->
-        <!--    <br><span style="font-size:10px; text-align:right;">Laboratoire d’Anatomie Pathologique</span>-->
-        <!--</div> --}}-->
     </div>
 
     <div
