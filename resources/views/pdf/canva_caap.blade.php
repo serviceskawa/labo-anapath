25915387b9e14ce131743657e7cd18be6164f824
DevZkc20 <cyrille@wallyskak.com>
2024-04-08 13:36:37 +0100

    MIse a jour du fichier canva

--- resources/views/pdf/canva_caap.blade.php
+++ resources/views/pdf/canva_caap.blade.php
@@ -129,6 +129,7 @@ date_default_timezone_set('Europe/Paris');
             </tr>
             @endif --}}
 
+
             @if ($status == 1)
             <tr>
                 <td style="text-align: left; width: 33%; vertical-align: bottom;">
