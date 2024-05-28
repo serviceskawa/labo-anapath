1dce1c3fe4f8c3841e422eb1f1d346cfbeb19241
DevZkc20 <cyrille@wallyskak.com>
2024-05-28 08:51:32 +0100

    Mise a jour du code validation de la facture

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -724,11 +724,12 @@ class ReportController extends Controller
                             $query->where('code', 'like', '%' . $request->get('contenu') . '%');
                         })
                         ->orwhere('description', 'like', '%' . $request->get('contenu') . '%')
-                        ->orwhereHas('patient', function ($query) use ($request) {
-                            $query
-                                ->where('firstname', 'like', '%' . $request->get('contenu') . '%')
+                        ->orwhereHas('order', function ($query) use ($request) {
+                            $query->whereHas('patient', function ($query) use ($request) {
+                               $query->where('firstname', 'like', '%' . $request->get('contenu') . '%')
                                 ->orwhere('code', 'like', '%' . $request->get('contenu') . '%')
                                 ->orwhere('lastname', 'like', '%' . $request->get('contenu') . '%');
+                            });
                         });
                 }
 
